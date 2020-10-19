8f7c5be35422233bfcce90f7654c62e2b6c6f52f
Filmaster <36028787+Filmaster@users.noreply.github.com>
2020-10-19 10:04:09 +0200

    Update main.cpp

--- éespéčko/src/main.cpp
+++ éespéčko/src/main.cpp
@@ -11,9 +11,11 @@ const char* password = "dusikovi";
 // Set LED GPIO - tohle by mela byt ledka na esp32
 const int ledPin = 26;
 const int ledPin2=27;
-// Stores LED state
+const char* ntpServer = "pool.ntp.org";
 String ledState;
 String ledState2;
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -36,9 +38,35 @@ String processor(const String& var){
   return String();
 }
 
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  
+}
 
+void setup()
+{
+  Serial.begin(115200);
+  
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
  
-void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
@@ -85,8 +113,8 @@ void setup(){
   server.begin();
 }
  void loop(){
-
-
+  delay(1000);
+  printLocalTime();
  }
 
   
