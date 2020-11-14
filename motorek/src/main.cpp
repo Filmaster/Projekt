ea7d1c7a3dddc72ebacf77e36fb5a955c3cf17dc
Filmaster <36028787+Filmaster@users.noreply.github.com>
2020-11-14 18:54:20 +0100

    0.4

---
+++ motorek/src/main.cpp
@@ -0,0 +1,128 @@
+#include <Arduino.h>
+
+// Krokový motor a driver
+
+// čísla pinů pro digitální výstupy
+const int in1 =  3;
+const int in2 =  1;
+const int in3 = 22;
+const int in4 = 23;
+
+// proměnná pro nastavení rychlosti,
+// se zvětšujícím se číslem se rychlost zmenšuje
+int rychlost = 1;
+//
+int uhel = 360;
+
+void setup() {
+  // inicializace digitálních výstupů
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+
+
+// zde následují funkce pro volání jednotlivých
+// kroků pro otočení po či proti směru hodinových
+// ručiček
+
+
+
+
+// každý krok obsahuje výrobcem dané pořadí
+// pro správné spínání motoru a následnou
+// pauzu, kterou určujeme rychlost otáčení
+void krok1(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  delay(rychlost);
+}
+void krok2(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  delay(rychlost);
+}
+void krok3(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  delay(rychlost);
+}
+void krok4(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(rychlost);
+}
+void krok5(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(rychlost);
+}
+void krok6(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, HIGH);
+  delay(rychlost);
+}
+void krok7(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(rychlost);
+}
+void krok8(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(rychlost);
+}
+
+void rotacePoSmeru() {
+  krok1();
+  krok2();
+  krok3();
+  krok4();
+  krok5();
+  krok6();
+  krok7();
+  krok8();
+}
+
+void rotaceProtiSmeru() {
+  krok8();
+  krok7();
+  krok6();
+  krok5();
+  krok4();
+  krok3();
+  krok2();
+  krok1();
+}
+
+
+void loop() {
+  for(int i=0;i<(uhel*64/45);i++){
+    rotacePoSmeru();
+  }
+  // pauza po dobu 1 vteřiny
+  delay(1000);
+  
+  for(int i=0;i<(uhel*64/45);i++){
+    rotaceProtiSmeru();
+  }
+  // pauza po dobu 1 vteřiny
+  delay(1000);
+}
